f098f1bb45a4dbc1bbb3eb468bbc7a17d8e7216e
Kamina <37193227+Segmentree@users.noreply.github.com>
2024-01-14 01:02:56 +0100

    handling canvas and draw one hexagon

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,23 @@
-console.log('Loaded index.js');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const angle = (2 * Math.PI) / 6;
+const radius = 50;
+
+function drawHexagon(x, y) {
+  ctx.beginPath();
+  for (let i = 0; i < 6; i++) {
+    ctx.lineTo(
+      x + radius * Math.cos(angle * i),
+      y + radius * Math.sin(angle * i)
+    );
+  }
+  ctx.closePath();
+  ctx.stroke();
+}
+
+function init() {
+  drawHexagon(100, 100);
+}
+
+init();
